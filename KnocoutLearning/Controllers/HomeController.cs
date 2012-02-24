ec82147c3095e0fca90927cc60323006d2a13616
Antonio Junior <agcjunior@gmail.com>
2012-02-24 14:51:05 -0300

    primeira versao

---
+++ KnocoutLearning/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KnocoutLearning.Models;
+
+namespace KnocoutLearning.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Save(Person person)
+        {
+            var message = string.Format("Registro gravado {0} {1}", person.FirstName, person.LastName);
+            return Json(new { message });
+        }
+
+    }
+}
---
+++ KnocoutLearning/Models/Friend.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnocoutLearning.Models
+{
+    public class Friend
+    {
+        public string Name { get; set; }
+        public bool IsOnTwitter { get; set; }
+        public string TwitterName { get; set; }
+    }
+}
